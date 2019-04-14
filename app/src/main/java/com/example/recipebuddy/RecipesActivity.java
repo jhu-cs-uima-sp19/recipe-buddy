7da247be56799f773d4163e3126a1caf45bc7ab6
Kevin <kpeng7@jhu.edu>
2019-04-14 17:40:56 -0400

    Working on recipes generation

--- app/src/main/java/com/example/recipebuddy/RecipesActivity.java
+++ app/src/main/java/com/example/recipebuddy/RecipesActivity.java
@@ -1,16 +1,24 @@
 package com.example.recipebuddy;
 
+import android.content.Intent;
+import android.graphics.Canvas;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.View;
 import android.widget.ImageButton;
+
+import java.util.ArrayList;
 
 public class RecipesActivity extends AppCompatActivity {
 
     private static ImageButton cancel;
+    private ArrayList<ItemsListSingleItem> data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +34,41 @@ public class RecipesActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        String[] items = {"Chicken", "Beef", "Pork", "Lamb", "Turkey"};
+        data = createItemsList(items);
+
+        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerViewRecipes);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+        // specify an adapter (see also next example)
+        RecipesListAdapter adapter = new RecipesListAdapter(this, data, new CustomItemClickListener() {
+            @Override
+            public void onItemClick(View v, int position) {
+                Intent intent = new Intent(v.getContext(), DisplayRecipeActivity.class);
+                intent.putExtra("name", data.get(position).getTitle());
+                startActivity(intent);
+            }
+        });
+        recyclerView.setAdapter(adapter);
     }
 
+    public ArrayList<ItemsListSingleItem> createItemsList(String[] list) {
+        ArrayList<ItemsListSingleItem> out = new ArrayList<>();
+        for (int i = 0; i < list.length; i++) {
+            out.add(new ItemsListSingleItem(
+                    i + 1,
+                    list[i],
+                    ""
+            ));
+        }
+        return out;
+    }
 }
---
+++ app/src/main/java/com/example/recipebuddy/RecipesListAdapter.java
@@ -0,0 +1,86 @@
+package com.example.recipebuddy;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.text.Html;
+import android.text.TextUtils;
+import android.util.SparseBooleanArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecipesListAdapter extends RecyclerView.Adapter<RecipesListAdapter.ViewHolder> {
+    ArrayList<ItemsListSingleItem> data;
+
+    Context mContext;
+    CustomItemClickListener listener;
+    SparseBooleanArray selectedItems = new SparseBooleanArray();
+
+    public RecipesListAdapter(Context context, ArrayList<ItemsListSingleItem> itemList) {
+        this.data = itemList;
+        this.mContext = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View mView = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_ingredient, parent, false);
+        final ViewHolder mViewHolder = new ViewHolder(mView);
+        mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.onItemClick(v, mViewHolder.getPosition());
+                if (selectedItems.get(mViewHolder.getAdapterPosition(), false)) {
+                    selectedItems.delete(mViewHolder.getAdapterPosition());
+                    v.findViewById(R.id.view_foreground).setSelected(false);
+                }
+                else {
+                    selectedItems.put(mViewHolder.getAdapterPosition(), true);
+                    v.findViewById(R.id.view_foreground).setSelected(true);
+                }
+            }
+        });
+        return mViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.itemTitle.setText(Html.fromHtml(data.get(position).getTitle()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public RecipesListAdapter(Context mContext, ArrayList<ItemsListSingleItem> data, CustomItemClickListener listener) {
+        this.data = data;
+        this.mContext = mContext;
+        this.listener = listener;
+    }
+
+    public SparseBooleanArray getSelectedItems() {
+        return selectedItems;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView itemTitle;
+        public ImageView thumbnailImage;
+
+        ViewHolder(View v) {
+            super(v);
+            itemTitle = (TextView) v
+                    .findViewById(R.id.name);
+            thumbnailImage = (ImageView) v.findViewById(R.id.thumbnail);
+        }
+    }
+}
+
