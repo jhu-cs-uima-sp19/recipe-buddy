b49ef6231863d75a778326849eb00f49c2ac538f
jz1242 <jz1242@gmail.com>
2019-04-14 20:37:13 -0400

    Separate table constants into new file

---
+++ app/src/main/java/com/example/recipebuddy/DBConstants.java
@@ -0,0 +1,13 @@
+package com.example.recipebuddy;
+
+import android.provider.BaseColumns;
+
+public class DBConstants {
+
+    public static final class IngredientEntry implements BaseColumns {
+        public static final String TABLE_NAME = "groceryList";
+        public static final String COLUMN_NAME = "name";
+        public static final String COLUMN_AMOUNT = "amount";
+        public static final String COLUMN_TIMESTAMP = "timestamp";
+    }
+}
