61fb0f55263acee482e6d119beab82877fc1a799
jz1242 <jz1242@gmail.com>
2019-04-14 21:07:49 -0400

    Main kitchen db handler created

--- app/src/main/java/com/example/recipebuddy/DBConstants.java
+++ app/src/main/java/com/example/recipebuddy/DBConstants.java
@@ -4,10 +4,9 @@ import android.provider.BaseColumns;
 
 public class DBConstants {
 
-    public static final class IngredientEntry implements BaseColumns {
-        public static final String TABLE_NAME = "groceryList";
+    public static final class KitchenColumns implements BaseColumns {
+        public static final String TABLE_NAME = "kitchen";
         public static final String COLUMN_NAME = "name";
-        public static final String COLUMN_AMOUNT = "amount";
         public static final String COLUMN_TIMESTAMP = "timestamp";
     }
 }
